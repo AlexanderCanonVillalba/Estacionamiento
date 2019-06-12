ea2143aebd6a57a27eb2f8327eca5122b4c0ecd8
Erney Alexander Cañon Villalba - Ceiba Software <erney.canon@ceiba.com.co>
2019-06-12 13:56:58 -0500

    test ticket

--- src/test/java/co/com/ceiba/parking/test/databuilder/TicketDataBuilder.java
+++ src/test/java/co/com/ceiba/parking/test/databuilder/TicketDataBuilder.java
@@ -5,15 +5,36 @@ import java.util.Date;
 public class TicketDataBuilder {
 
 	
-	private long id ;
-	private long vehiculoid ;
+	private  long id ;
+	private  long vehiculoid ;
 	private Date fechaini  ;
 	private Date fechafin  ;
 	private double precio  ;
 	
 	
-	private TicketDataBuilder id(long id) { 		
+	public  TicketDataBuilder id(long id) { 		
 		this.id =  id  ;
 		return  this ;
+	}
+	
+	public TicketDataBuilder vehiculoid(long vehiculoid) {
+		this.vehiculoid = vehiculoid; 
+		return this;
+		
+	}
+	
+	public TicketDataBuilder fechaini(Date fechaini) {
+		this.fechaini = fechaini;
+		return this  ;
+	}
+	
+	public TicketDataBuilder fechafin(Date fechafin) {
+		this.fechafin = fechafin;
+		return this;
+	}
+	
+	public TicketDataBuilder precio(double precio) {
+		this.precio = precio  ;
+		return this ;
 	}
 }
--- src/test/java/co/com/ceiba/parking/test/domain/TicketTest.java
+++ src/test/java/co/com/ceiba/parking/test/domain/TicketTest.java
@@ -1,12 +1,28 @@
 package co.com.ceiba.parking.test.domain;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Calendar;
+
 import org.junit.Test;
+
+import co.com.ceiba.parking.test.databuilder.TicketDataBuilder;
+
 
 public class TicketTest {
 
 	
 	@Test
 	public void validarTicket() {
+		
+		
+		TicketDataBuilder ticketDataBuilder  = new TicketDataBuilder();
+		
+		ticketDataBuilder.id(1)
+				.vehiculoid(1).fechafin(Calendar.getInstance().getTime()).precio(8000);
+		
+		assertNotNull(ticketDataBuilder);
+				
 		
 	}
 }
