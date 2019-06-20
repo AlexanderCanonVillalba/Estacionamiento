d011d32a896ec2fa604b313f11448e7b8a2b2a18
Erney Alexander Cañon Villalba - Ceiba Software <erney.canon@ceiba.com.co>
2019-06-20 14:37:36 -0500

    parquear test

--- src/test/java/co/com/ceiba/parking/test/domain/ParquearTest.java
+++ src/test/java/co/com/ceiba/parking/test/domain/ParquearTest.java
@@ -2,14 +2,25 @@ package co.com.ceiba.parking.test.domain;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 import co.com.ceiba.parking.test.databuilder.VehiculoDataBuilder;
+import co.com.ceiba.parqueadero.domain.Parquear;
 import co.com.ceiba.parqueadero.domain.Vehiculo;
 
 public class ParquearTest {
 	
-	
-
+	@Test
+	public void  registrarEntrada() {
+		Vehiculo vehiculo = new Vehiculo();
+		Parquear parquear = new Parquear();
+		vehiculo.setPlaca("ACB-000");
+		vehiculo.setTipovehiculo("Carro");
+		vehiculo.setFechaini(new Date());
+		parquear.registrarEntrada(vehiculo);
+	}
+     
 	
 }
